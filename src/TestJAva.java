b6391332157ebd9c31f967622813227aeeffa53c
Sungbo Shim <ishimsungbo@outlook.com>
2019-05-28 18:20:05 +0900

    1.1
    엥?

--- src/TestJAva.java
+++ src/TestJAva.java
@@ -1,6 +1,8 @@
 public class TestJAva {
 
     public static void main(String[] args){
+
+        System.out.println("최초 git을 하기 위한 ...셋팅끝...");
         System.out.println("최초 git을 하기 위한 ...셋팅끝...");
     }
 
